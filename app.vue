a818b18bd6adaf27c83fc13cf7d363aeb32188ef
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-04-19 13:16:54 +0200

    feat: fetch data from Supabase

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    {{ data }}
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const client = useSupabaseClient()
+
+const { data } = await useAsyncData('images', async () => client.from('images').select('*').order('created_at'), { transform: result => result.body })
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,6 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
   modules: [
     "@nuxtjs/tailwindcss",
-    ['@nuxtjs/supabase', {
-      url: process.env.SUPABASE_URL,
-      key: process.env.SUPABASE_KEY
-    }]
+    "@nuxtjs/supabase"
   ]
 })
