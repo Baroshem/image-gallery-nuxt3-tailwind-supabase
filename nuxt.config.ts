86b4b39aacb445acdab1c814c604b2684ea79c72
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-04-19 12:55:23 +0200

    feat: add Supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: [
+    "@nuxtjs/tailwindcss",
+    ['@nuxtjs/supabase', {
+      url: process.env.SUPABASE_URL,
+      key: process.env.SUPABASE_KEY
+    }]
+  ]
 })
